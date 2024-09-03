9047921d93b632e129e35f7ed94581dc19298434
Lokinha92 <ghferreira92@gmail.com>
2024-09-03 15:31:32 -0300

    novos insights da tabela finances

--- node_sequelize/src/data-analisys/analise.py
+++ node_sequelize/src/data-analisys/analise.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # Conectar ao banco de dados:
 conexao = mysql.connector.connect(
@@ -15,7 +16,6 @@ conexao = mysql.connector.connect(
 # Ler dados da tabela produtos
 query = "SELECT * FROM products"
 df = pd.read_sql(query, conexao)
-conexao.close()
 
 # Exibição de alguns insights da tabela produtos -----------
 
@@ -45,6 +45,42 @@ fornecedores = produtos_por_forncedor['supplier'].tolist()
 #produtos em menor quantidade no estoque:
 
 menor_estoque = df.sort_values(by='quantity_per_unit', ascending= True)
+
+# insights da tabela finances: --------------------
+
+query_fin = "SELECT * FROM finance"
+df_fin = pd.read_sql(query_fin, conexao)
+conexao.close()
+
+# Operações por tipo: (compra ou venda)
+vendas = df_fin[df_fin['description'] == 'Venda']
+compras = df_fin[df_fin['description'] == 'Compra']
+total_vendas = vendas['value'].sum()
+total_compras = compras['value'].sum()
+
+select_compra_venda = df_fin['description'].tolist()[:2]
+
+
+ # Operações por metodo de pagamento
+ 
+select_payment_method = df_fin['payment_method'].tolist()[:4]
+
+dinheiro = df_fin[df_fin['payment_method'] == 'Dinheiro']
+total_din = dinheiro['value'].sum()
+
+credito = df_fin[df_fin['payment_method'] == 'Cartão de credito']
+total_cred = credito['value'].sum()
+
+pix = df_fin[df_fin['payment_method'] == 'Pix']
+total_pix = pix['value'].sum()
+
+deb = df_fin[df_fin['payment_method'] == 'Cartão de débito']
+total_deb = deb['value'].sum()
+
+# volume de operações por método de pagamento:
+
+agrupa = df_fin.groupby(['payment_method']).size()
+
    
 # ----- daqui pra cima, adicionar mais insights
 # ----- daqui pra baixo, criação do dashboard
@@ -81,3 +117,82 @@ col5, spacer, col6 = st.columns([1, 0.23, 1])
 with col5:
     st.header('10 produtos em menor quantidade no estoque:')
     st.dataframe(menor_estoque.head(10)[['name', 'quantity_per_unit']], hide_index=True, use_container_width= True)
+    
+with col6:
+    st.header("Operações por compra ou venda: ")
+    opr_selecionada = st.selectbox("selecione o tipo de opepração: ", select_compra_venda)
+    operacoes_tipo_selec = df_fin[df_fin['description'] == opr_selecionada]
+    st.dataframe(operacoes_tipo_selec[['product_id', 'description', 'value', 'quantity', 'payment_method']], hide_index=True, use_container_width= True)
+    
+    if opr_selecionada == 'Venda':
+        st.markdown(
+    f"""
+    <div style='text-align: center; padding-top: 10px; font-size: 12px; color: gray;'>
+        <hr>
+        <p>Valor total das transações de venda:</p>
+        <p>R$ {total_vendas:.2f}</p>
+    </div>
+    """, unsafe_allow_html=True
+    )
+    else:
+        st.markdown(
+    f"""
+    <div style='text-align: center; padding-top: 10px; font-size: 12px; color: gray;'>
+        <hr>
+        <p>Valor total das transações de compra:</p>
+        <p>R$ {total_compras:.2f}</p>
+    </div>
+    """, unsafe_allow_html=True
+    )
+        
+col7, spacer, col8 = st.columns([1, 0.23, 1])
+
+with col7:
+    st.header("Operações por método de pagamento: ")
+    opr_selecionada_pm = st.selectbox("selecione o método de pagamento: ", select_payment_method)
+    operacoes_met_selec = df_fin[df_fin['payment_method'] == opr_selecionada_pm]
+    st.dataframe(operacoes_met_selec[['product_id', 'description', 'value', 'quantity', 'payment_method']], hide_index=True, use_container_width= True)
+    if opr_selecionada_pm == 'Dinheiro':
+        st.markdown(
+    f"""
+    <div style='text-align: center; padding-top: 10px; font-size: 12px; color: gray;'>
+        <hr>
+        <p>Valor total das transações em dinheiro:</p>
+        <p>R$ {total_din:.2f}</p>
+    </div>
+    """, unsafe_allow_html=True
+    )
+    elif opr_selecionada_pm == 'Cartão de credito':
+        st.markdown(
+    f"""
+    <div style='text-align: center; padding-top: 10px; font-size: 12px; color: gray;'>
+        <hr>
+        <p>Valor total das transações no cartão de crédito:</p>
+        <p>R$ {total_cred:.2f}</p>
+    </div>
+    """, unsafe_allow_html=True
+    )
+    elif opr_selecionada_pm == 'Pix':
+        st.markdown(
+    f"""
+    <div style='text-align: center; padding-top: 10px; font-size: 12px; color: gray;'>
+        <hr>
+        <p>Valor total das transações via pix:</p>
+        <p>R$ {total_pix:.2f}</p>
+    </div>
+    """, unsafe_allow_html=True
+    )
+    else:
+        st.markdown(
+    f"""
+    <div style='text-align: center; padding-top: 10px; font-size: 12px; color: gray;'>
+        <hr>
+        <p>Valor total das transações no cartão de débito:</p>
+        <p>R$ {total_deb:.2f}</p>
+    </div>
+    """, unsafe_allow_html=True
+    )
+        
+with col8:
+    st.header("Volume de operações por método de pagamento: ")
+    st.bar_chart(agrupa)
