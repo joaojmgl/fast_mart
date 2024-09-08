8bfe4ea45377a5b3ff410545f181adfe6b8153c0
Lokinha92 <ghferreira92@gmail.com>
2024-09-08 18:05:45 -0300

    Dashboard preparado para integração

--- node_sequelize/src/controllers/AnalyticsController.js
+++ node_sequelize/src/controllers/AnalyticsController.js
@@ -1,61 +1,21 @@
-const { exec } = require('child_process');
-
-
+const { execSync } = require('child_process');
 
 module.exports = {
-    async executeAnalisys(res,req) {
-        comando_1 = "cd /home/jmgl/Documentos/fast_mart/node_sequelize/src/data-analisys && ls"
-        // args_1 = ["/home/jmgl/Documentos/fast_mart/node_sequelize/src/data-analisys"]
-        comando_2 = "streamlit run analise.py"
-        // args_2 = "analise.py"
-        try{
-            exec(comando_1, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`Erro ao executar o comando: ${error.message}`);
-                    return;
-                }
+    async executeAnalisys(req, res) {
+        try {
+            // Mudança de diretório e execução do script Python -- COLOCAR CAMINHO CORRETO PARA A PASTA DATA-ANALISYS AQUI (TEM QUE ESTAR ENTRE "")
+            execSync(`cd "C:/Users/Gustavo Henrique/Desktop/fast_mart/node_sequelize/src/data-analisys" && python start_server.py`, { stdio: 'inherit' });
 
-                if (stderr) {
-                    console.error(`Erro no terminal: ${stderr}`);
-                    return;
-                }
-
-                console.log(`Resultado: ${stdout}`);
-            });
             return res.status(200).send({
-                status: 0,
-            })
-    }catch(err) {
-            console.log(err);
-                // return res.status(500).send({
-                //     status: 0,
-                //     message:"Internal server erro."
-                // })
-
-        }try{
-            exec(comando_2, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`Erro ao executar o comando: ${error.message}`);
-                    return;
-                }
-
-                if (stderr) {
-                    console.error(`Erro no terminal: ${stderr}`);
-                    return;
-                }
-
-                console.log(`Resultado: ${stdout}`);
+                status: 1,
+                message: 'Comando executado com sucesso!',
             });
-            return res.status(200).send({
+        } catch (error) {
+            console.error(`Erro ao executar o comando: ${error.message}`);
+            return res.status(500).send({
                 status: 0,
-            })
-        }catch(err) {
-            console.log(err);
-            // return res.status(500).send({
-            //     status: 0,
-            //     message:"Internal server erro."
-            // })
-
+                message: `Erro ao executar o comando: ${error.message}`,
+            });
         }
-    }
-}
+    },
+};
---
+++ node_sequelize/src/data-analisys/start_server.py
@@ -0,0 +1,15 @@
+import subprocess
+
+# Comando para iniciar o servidor Streamlit -- COLOCAR CAMINHO CORRETO PARA O ARQUIVO DASHBOARD.PY AQUI!!!!
+comando = 'python -m streamlit run "C:/Users/Gustavo Henrique/Desktop/fast_mart/node_sequelize/src/data-analisys/dashboard.py" --server.port 8501 --server.headless true'
+
+# Função para executar o comando
+def iniciar_streamlit():
+    try:
+        subprocess.Popen(comando, shell=True)
+        print("Servidor Streamlit iniciado com sucesso.")
+    except Exception as e:
+        print(f"Erro ao iniciar o servidor Streamlit: {e}")
+
+if __name__ == "__main__":
+    iniciar_streamlit()
